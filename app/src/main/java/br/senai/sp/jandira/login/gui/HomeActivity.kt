dc503fc8043a310802a05151f9bf251856655d28
GUILHERME DE OLIVEIRA LIMA <22282086@dsn127m1249388.cfp127aluno.local>
2023-04-25 08:37:18 -0300

    add

--- app/src/main/java/br/senai/sp/jandira/login/gui/HomeActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/login/gui/HomeActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.login.ui.theme.LoginTheme
 
 class HomeActivity : ComponentActivity() {
@@ -33,26 +40,9 @@ class HomeActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    Column(modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Red))
-                    {
-                        Column(modifier = Modifier
-                            .height(197.dp)
-                            .fillMaxWidth()
-                            .background(Color.Cyan)) {
 
-                            Row(
-                                modifier = Modifier.fillMaxWidth().background(Color.Black)
-                            ) {
+                    HomeScreen ("Android")
 
-
-
-                            }
-
-                        }
-
-                    }
                 }
             }
         }
@@ -60,14 +50,38 @@ class HomeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting3(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen(name: String) {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp).fillMaxWidth()
+                , shape = RectangleShape
+        ) {
+
+            Image(painter = painterResource(id = br.senai.sp.jandira.login.R.drawable.paris),
+                contentDescription = "logo",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(text = stringResource(id = br.senai.sp.jandira.login.R.string.categories),
+            color = Color (56, 54, 54 ),
+            fontSize = 14.sp,
+            modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+        )
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
+
 @Composable
 fun DefaultPreview3() {
     LoginTheme {
-        Greeting3("Android")
+        HomeScreen ("Android")
     }
 }
